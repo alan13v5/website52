d571d9307f06d27115a353a23fee32213a6d06f6
Juan David SG <david021202@gmail.com>
2024-04-16 09:51:04 -0600

    App de boton Agregado

---
+++ Deivid-SG/JavaScript_Apps/App_Boton/js/app.js
@@ -0,0 +1,13 @@
+const Caja = document.getElementById("caja")
+const Boton = document.getElementById("btnEjecutar")
+
+
+Boton.addEventListener("click", e => {
+    e.preventDefault();
+    Caja.value = "Examen";
+    Caja.maxLength = 6;
+    Caja.style.border = "5px solid blue";
+    Caja.style.borderRadius = "20px";
+    Caja.style.fontSize = "50px";
+});
+
