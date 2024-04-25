d55678cd2d38f64db8b03dae24be7fb6fe739a12
Nelly Abigail Orozco Vazquez <vzzgalavis@gmail.com>
2024-04-24 23:20:17 -0600

    Se modificaron los archivos

--- ellyvzz12/js/appsnumperfectos.js
+++ ellyvzz12/js/appsnumperfectos.js
@@ -1,3 +1,26 @@
-const numPer = document.getElementById("numPer");
-const boton = document.getElementById("btnVer");
+const num = document.getElementById("num");
+const btnVer = document.getElementById("btnVer");
 const lista = document.getElementById("lista");
+
+btnVer.addEventListener("click", e => {
+    e.preventDefault();
+
+    let numero = Number(num.value);
+    let numPer = [];
+    let cant = 2;
+    while (numPer.length < numero){
+        let sumaDivisores = 1;
+        for (let i=2; i<= Math.sqrt(cant); i++) {
+            if (cant % i === 0){
+                sumaDivisores += i;
+                if (i !== cant / i){
+                    sumaDivisores += cant / i;
+                }
+            }
+        }
+        if(sumaDivisores === cant){
+            numPer.push(cant);
+        }
+        cant++;
+    };
+});
