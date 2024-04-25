8e31476897846a0a757e2e1c0a12b849fa171d77
Katia Aguilar G <katia.aguilarg@uanl.edu.mx>
2024-04-24 20:57:15 -0600

    se agrego actividad tarea num perfectos

---
+++ KatiaAguilar/Java/numperfectos/numperfectos.js
@@ -0,0 +1,31 @@
+const numper = document.getElementById("num");
+const boton = document.getElementById("btnVer");
+const lista = document.getElementById("lista");
+
+boton.addEventListener("click", e => {
+    e.preventDefault();
+
+    const np = parseInt(num.value);
+    let num=1;
+    let encontrados=0;
+
+    while(encontrados<np){
+        if(encontrados==0){
+            lista.innerHTML="";
+        }
+        let suma=0;
+        for(let i =1; i< num; i++){
+            if(num%i==0){
+                suma += i;
+            }
+        }
+
+        if(suma===num){
+            const item = document.createElement('li');
+            item.textContent=num;
+            lista.appendChild(item);
+            encontrados++;
+        }
+        num++;
+    }
+});
